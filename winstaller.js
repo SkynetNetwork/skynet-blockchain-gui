38613470d482978e258668f45359536bf45e275a
Ugo Ridolfi <u.ridolfi@gmail.com>
2021-09-07 14:15:08 +0200

    minor changes

--- winstaller.js
+++ winstaller.js
@@ -18,7 +18,7 @@ function getInstallerConfig () {
     authors: 'Skynet Network',
     version: process.env.SKYNET_INSTALLER_VERSION,
     noMsi: true,
-    iconUrl: 'https://raw.githubusercontent.com/SkynetNetwork/skynet-blockchain-gui/c219842697bf1c1fdd487d48ddf6d8c61d391b6f/src/assets/img/skynet_tb.ico',
+    iconUrl: 'https://raw.githubusercontent.com/SkynetNetwork/skynet-blockchain-gui/main/src/assets/img/skynet_tb.ico',
     outputDirectory: path.join(outPath, 'windows-installer'),
     certificateFile: 'win_code_sign_cert.p12',
     certificatePassword: process.env.WIN_CODE_SIGN_PASS,
